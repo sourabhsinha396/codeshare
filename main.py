ec4fcfe09b5c7a573393225dbdc042c7b7d75bd5
sourabh27 <sourabh@chat360.io>
2023-07-26 22:11:12 +0530

    first websocket route

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI, WebSocket
+
 
 app = FastAPI()
+
+
+@app.websocket("/ws")
+async def health_check(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_json({"msg":data})
