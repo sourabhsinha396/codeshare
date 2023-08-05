c152fdc3cb307a0ae6c2f584f2833effa1315323
sourabh27 <sourabh@chat360.io>
2023-08-05 20:13:26 +0530

    add realtime codesharing capability

--- main.py
+++ main.py
@@ -1,56 +1,41 @@
+from broadcaster import Broadcast
+
 from fastapi import FastAPI, WebSocket, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.concurrency import run_until_first_complete
 
+
+broadcast = Broadcast("redis://redis:6379")
 templates = Jinja2Templates(directory="templates")
-app = FastAPI()
+app = FastAPI(on_startup=[broadcast.connect], on_shutdown=[broadcast.disconnect])
 
 
-@app.get("/{room_id}", response_class=HTMLResponse)
-async def chatroom(request: Request, room_id: str):
-    context = {"request": request, "room_id": room_id}
-    return templates.TemplateResponse("chatroom.html", context)
+@app.get("/{room_id}/", response_class=HTMLResponse)
+async def get(request: Request, room_id:str):
+    context = {"room_id":room_id,"request":request}
+    return templates.TemplateResponse("codeshare.html",context)
 
 
-class ConnectionManager:
-    def __init__(self) -> None:
-        self.active_connections: list[str,WebSocket]= {}
-        print("Creating a list to hold active connections",self.active_connections)
+async def chatroom_ws_receiver(websocket: WebSocket, room_id: str):
+    async for message in websocket.iter_text():
+        await broadcast.publish(channel=f"chatroom_{room_id}", message=message)
 
-    async def connect(self, room_id: str, websocket: WebSocket):
-        await websocket.accept()
-        if not self.active_connections.get(room_id):
-            self.active_connections[room_id] = []
-        self.active_connections[room_id].append(websocket)
-        print("New Active connections are ",self.active_connections)
 
-    async def disconnect(self, room_id: str, websocket: WebSocket):
-        self.active_connections[room_id].remove(websocket)
-        print("After disconnect active connections are: ",self.active_connections)
-
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
-        print("Sent a personal msg to , ",websocket)
-
-    async def broadcast(self, message: str, room_id: str, websocket: WebSocket):
-        for connection in self.active_connections[room_id]:
-            if connection != websocket:
-                await connection.send_text(message)
-                print("In broadcast: sent msg to ",connection)
-
-manager = ConnectionManager()
+async def chatroom_ws_sender(websocket: WebSocket, room_id: str):
+    async with broadcast.subscribe(channel=f"chatroom_{room_id}") as subscriber:
+        async for event in subscriber:
+            await websocket.send_text(event.message)
 
 
 @app.websocket("/{room_id}")
 async def websocket_chat(websocket: WebSocket, room_id: str):
-    await manager.connect(room_id, websocket)
-    try:
-        while True:
-            data = await websocket.receive_text()
-            await manager.send_personal_message(f"You wrote: {data}",websocket)
-            await manager.broadcast(f"A client says: {data}", room_id, websocket)
-    except Exception as e:
-        print("Got an exception ",e)
-        await manager.disconnect(room_id, websocket)
+    await websocket.accept()
+    await run_until_first_complete(
+        (chatroom_ws_receiver, {"websocket": websocket, "room_id":room_id}),
+        (chatroom_ws_sender, {"websocket": websocket, "room_id":room_id}),
+    )
+
+
 
 
